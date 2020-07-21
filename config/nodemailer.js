0722179bf11869afed605460e58a9fb9e49031e0
DhanushM12 <dhanushmukhi@gmail.com>
2020-07-21 22:08:28 +0530

    added nodemailer configuration to sent mails for reset password and confirmation

---
+++ config/nodemailer.js
@@ -0,0 +1,18 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+const env = require("./environment");
+
+//defining transporter
+// create reusable transporter object using the default SMTP transport
+//this is the part which sends email and how communication takes place
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  post: 587,
+  source: false,
+  auth: {
+    user: "dhanushmukhi",
+    pass: "",
+  },
+});
